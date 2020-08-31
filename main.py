53452a87dc586cc25902f69296da493b2c910490
Corey Minter <corey.minter@gmail.com>
2020-08-30 19:46:38 -0700

    added oppose fill/clear

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+mode = 0
+
 def fill1(mode, brightness=255):
     for i in range(5):
         for j in range(5):
@@ -5,7 +7,47 @@ def fill1(mode, brightness=255):
             led.plot_brightness(col, row, brightness)
             basic.pause(50)
 
-def on_forever():
-    for brightness in [255, 0]:  fill1(randint(0, 1), brightness)
+def spiral1(brightness=255):
+    for i in range(3):
+        led.plot_brightness(i, 0, brightness)
+        led.plot_brightness(4, i, brightness)
+        led.plot_brightness(4-i, 4, brightness)
+        led.plot_brightness(0, 4-i, brightness)
+        basic.pause(100)
+    for i in range(0, 2):
+        led.plot_brightness(i, 1, brightness)
+        led.plot_brightness(3, i, brightness)
+        led.plot_brightness(4-i, 3, brightness)
+        led.plot_brightness(1, 4-i, brightness)
+        basic.pause(100)
+    for i in range(1, 2):
+        led.plot_brightness(i, 2, brightness)
+        led.plot_brightness(2, i, brightness)
+        led.plot_brightness(4-i, 2, brightness)
+        led.plot_brightness(2, 4-i, brightness)
+        basic.pause(100)
+    led.plot_brightness(2, 2, brightness)
+    basic.pause(100)
 
+def oppose1(mode, brightness=255):
+    for i in range(5):
+        for jEven in [0, 2, 4]:
+            (row, col) = (i, jEven) if mode == 0 else (jEven, i)
+            led.plot_brightness(col, row, brightness)
+        for jOdd in [1, 3]:
+            (row, col) = (4-i, jOdd) if mode == 0 else (jOdd, 4-i)
+            led.plot_brightness(col, row, brightness)
+        basic.pause(100)
+
+def on_forever():
+    for brightness in [255, 0]:  
+        if mode == 0:  fill1(randint(0, 1), brightness)
+        elif mode == 1:  spiral1(brightness)
+        elif mode == 2:  oppose1(randint(0, 1), brightness)
+
+def on_button_pressed_a():
+    global mode
+    mode += 1
+    if mode > 2:  mode = 0
+input.on_button_pressed(Button.A, on_button_pressed_a)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let mode = 0
 function fill1(mode: number, brightness: number = 255) {
     for (let i = 0; i < 5; i++) {
         for (let j = 0; j < 5; j++) {
@@ -8,8 +9,64 @@ function fill1(mode: number, brightness: number = 255) {
     }
 }
 
+function spiral1(brightness: number = 255) {
+    let i: number;
+    for (i = 0; i < 3; i++) {
+        led.plotBrightness(i, 0, brightness)
+        led.plotBrightness(4, i, brightness)
+        led.plotBrightness(4 - i, 4, brightness)
+        led.plotBrightness(0, 4 - i, brightness)
+        basic.pause(100)
+    }
+    for (i = 0; i < 2; i++) {
+        led.plotBrightness(i, 1, brightness)
+        led.plotBrightness(3, i, brightness)
+        led.plotBrightness(4 - i, 3, brightness)
+        led.plotBrightness(1, 4 - i, brightness)
+        basic.pause(100)
+    }
+    for (i = 1; i < 2; i++) {
+        led.plotBrightness(i, 2, brightness)
+        led.plotBrightness(2, i, brightness)
+        led.plotBrightness(4 - i, 2, brightness)
+        led.plotBrightness(2, 4 - i, brightness)
+        basic.pause(100)
+    }
+    led.plotBrightness(2, 2, brightness)
+    basic.pause(100)
+}
+
+function oppose1(mode: number, brightness: number = 255) {
+    for (let i = 0; i < 5; i++) {
+        for (let jEven of [0, 2, 4]) {
+            let [row, col] = mode == 0 ? [i, jEven] : [jEven, i]
+            led.plotBrightness(col, row, brightness)
+        }
+        for (let jOdd of [1, 3]) {
+            let [row, col] = mode == 0 ? [4 - i, jOdd] : [jOdd, 4 - i]
+            led.plotBrightness(col, row, brightness)
+        }
+        basic.pause(100)
+    }
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    mode += 1
+    if (mode > 2) {
+        mode = 0
+    }
+    
+})
 basic.forever(function on_forever() {
     for (let brightness of [255, 0]) {
-        fill1(randint(0, 1), brightness)
+        if (mode == 0) {
+            fill1(randint(0, 1), brightness)
+        } else if (mode == 1) {
+            spiral1(brightness)
+        } else if (mode == 2) {
+            oppose1(randint(0, 1), brightness)
+        }
+        
     }
 })
